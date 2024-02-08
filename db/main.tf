f9d592e609934ad2977bebed8bde3799c5cdf036
kmkouokam <94249111+kmkouokam@users.noreply.github.com>
2024-02-08 13:30:05 -0500

    Update main.tf

--- db/main.tf
+++ db/main.tf
@@ -1,21 +1,5 @@
 
-
-terraform {
-  required_providers {
-    local = {
-      source = "hashicorp/local"
-    }
-
-    random = {
-      source = "hashicorp/random"
-    }
-
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
-
+    
 
 
 
