ab9f200ee2bd9e8ea5569191ff9de3506ab2d042
kmkouokam <94249111+kmkouokam@users.noreply.github.com>
2024-02-08 13:48:08 -0500

    Update main.tf

--- db/main.tf
+++ db/main.tf
@@ -1,6 +1,20 @@
 
     
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
 
+    random = {
+      source = "hashicorp/random"
+    }
+
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
 
 
 # After creating my app sg
